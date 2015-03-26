4cbb3b6af98bdfe698f2f2c7c760a673a2de6414
Brian Ji <jibrianh@gmail.com>
2015-03-26 16:13:13 -0700

    refactored player object

--- js/player.js
+++ js/player.js
@@ -1,24 +1,22 @@
 var playerID = 0;
 var players = [];					// pushing the players inside this empty array
-
 																 // class is player 
-function Player(avatar, flickrPhotoCount, hashtagCount) {        // every player is going to be a new player
-    this.totalHealth;
+function Player(obj) {        // every player is going to be a new player
     this.health = null;											 // health these are the properties of the class
     this.strength = null;  										 // speed etc.
     this.speed = null;				
     this.army = createArmy(flickrPhotoCount, hashtagCount);
     this.playerID = playerID++;
-    this.avatar = avatar;
-    this.flickrPhotoCount = flickrPhotoCount;
-    this.hashtagCount = hashtagCount;
     
+    for (var key in obj) {
+        this[key] = obj[key];
+    }    
 }
 
 function createPlayer(obj) {									// were grabbing teh certain value from the words
-    Promise.resolve(getTwitter()).then(function(obj) {			// if the getTwitter is loaded
-        Promise.resolve(getFlickr(obj)).then(function(obj) {	// if the getFlickr is loaded
-            players.push(new Player(obj['avatar'], obj['flickrPhotoCount'], obj['hashtagCount']));	// create the object when both of the top stuff are loaded
+    Promise.resolve( getTwitter() ).then(function(obj) {			// if the getTwitter is loaded
+        Promise.resolve( getFlickr(obj) ).then(function(obj) {	// if the getFlickr is loaded
+            players.push( new Player(obj) );	// create the object when both of the top stuff are loaded
         });
     });
 }
