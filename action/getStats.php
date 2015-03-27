264bbc67e9d1e5c98532a92f6959d7afd22571e3
Ahad Wasim <ahadwasim@gmail.com>
2015-03-27 15:57:02 -0700

    I have styled the contents

---
+++ action/getStats.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+// require the $CONN from the database;
+
+	$query = "SELECT * FROM users WHERE user_id = $user_id"
+
+	$user_id = 
+
+?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <link rel="stylesheet" href="style.css">
+<link rel = 'stylesheet' href = 'views/temp/css/animate.css'>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="js/main.js"></script>
@@ -9,6 +10,67 @@
 <script src="js/battle.js"></script>
 <script src="js/create.js"></script>
 
+
+	
+
+	<style>
+		body{
+			background: url('http://4.bp.blogspot.com/-Y6bxhFuK13s/UZx6gVxe7PI/AAAAAAAAAHo/nfC_u2c3dv8/s640/EndCredits2560x1600-16bit.jpg');
+			
+			color:#17E021;
+		}
+		.main_header{
+			border:1px solid black;
+			text-align: center;
+			color:white;
+		}
+
+		.container{
+			border:1px solid black;
+		}
+
+		.player1{
+			border:1px solid rgb(17, 45, 46);;
+
+		}
+
+		.first_player_contents{
+			border:1px solid red;
+		}
+
+		.player2{
+			border:1px solid blue;
+		}
+
+		.second_player_contents{
+			border:1px solid red;
+		}
+		
+		.dice{
+			border:1px solid red;
+			text-align: center;
+		}
+
+		#player1_image{
+			height:35vh;
+			width:100%;
+			
+		}
+
+		#player2_image{
+			height:35vh;
+			width:100%;	
+		}
+
+		.round_number{
+			border:1px solid white;
+			text-align: center;
+		}
+
+	</style>
+
+
+	
 <body>
 	<div id="login-container">
 		<?php include_once( 'views/login.php'); ?>
@@ -19,6 +81,10 @@
 	<div id="create-avatar-container">
 		<?php include_once( 'views/create-avatar.php'); ?>
 	</div>
+	<div id="login-container">
+		<?php include_once( 'views/temp/page3.php'); ?>
+	</div>
+	
 </body>
 
 </html>
